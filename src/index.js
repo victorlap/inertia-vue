cf6ad05a2ab33f45347008eb2e9623738cf24deb
Jonathan Reinink <jonathan@reinink.ca>
2019-04-24 14:37:06 -0400

    Rename "inertia-link" file to "link"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Inertia from 'inertia'
 import App from './app'
-import InertiaLink from './inertia-link'
+import InertiaLink from './link'
 
 export default App
 export { Inertia, InertiaLink }
