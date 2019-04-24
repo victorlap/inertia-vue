3058e46f66d318767fcb7d239813f0577e75e2c1
Jonathan Reinink <jonathan@reinink.ca>
2019-04-24 14:37:22 -0400

    Add remember mixin

---
+++ src/remember.js
@@ -0,0 +1,39 @@
+import Inertia from 'inertia'
+
+export default {
+  created() {
+    if (!this.$options.remember) {
+      return
+    }
+
+    if (Array.isArray(this.$options.remember)) {
+      this.$options.remember = {
+        data: this.$options.remember,
+      }
+    }
+
+    if (typeof this.$options.remember === 'string') {
+      this.$options.remember = {
+        data: [this.$options.remember],
+      }
+    }
+
+    const stateKey = this.$options.remember.key instanceof Function
+      ? this.$options.remember.key()
+      : this.$options.remember.key
+
+    const restored = Inertia.restore(stateKey)
+
+    this.$options.remember.data.forEach(key => {
+      if (restored !== undefined && restored[key] !== undefined) {
+        this[key] = restored[key]
+      }
+
+      this.$watch(key, data => {
+        let rememberedData = {}
+        this.$options.remember.data.forEach(key => rememberedData[key] = this[key])
+        Inertia.remember(rememberedData, stateKey)
+      }, { immediate: true, deep: true })
+    })
+  },
+}
