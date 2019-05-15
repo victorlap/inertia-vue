e600dd99cf600c7da1c9502b73b12f0104962b13
Michaël De Boey <info@michaeldeboey.be>
2019-05-15 17:17:02 +0200

    No comma in interfaces

--- index.d.ts
+++ index.d.ts
@@ -2,24 +2,24 @@ import Inertia, { Page as InertiaPage } from 'inertia'
 import { Component, FunctionalComponentOptions } from 'vue'
 
 interface Page<PageProps = {}> {
-  instance: Component | null,
-  props: PageProps | {},
+  instance: Component | null
+  props: PageProps | {}
 }
 interface AppData<PageProps = {}> {
-  page: Page<PageProps>,
+  page: Page<PageProps>
 }
 interface AppProps {
-  initialPage: InertiaPage,
-  resolveComponent: (name: string) => Component,
+  initialPage: InertiaPage
+  resolveComponent: (name: string) => Component
 }
 type App<PageProps = {}> = Component<AppData<PageProps>, never, never, AppProps>
 
 interface InertiaLinkProps {
-  href: string,
-  method?: string,
-  onClick?: () => void,
-  preserveScroll?: boolean,
-  replace?: boolean,
+  href: string
+  method?: string
+  onClick?: () => void
+  preserveScroll?: boolean
+  replace?: boolean
 }
 declare const InertiaLink: FunctionalComponentOptions<InertiaLinkProps>
 
