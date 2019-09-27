b2a4336d0930d3cf643c6dc09f4b01a21f3bacc6
Michaël De Boey <info@michaeldeboey.be>
2019-09-27 11:05:11 +0200

    Modernize typings

--- index.d.ts
+++ index.d.ts
@@ -1,45 +1,43 @@
+import * as Inertia from '@inertiajs/inertia'
 import { Component, FunctionalComponentOptions } from 'vue'
 
-declare namespace InertiaVue {
-  interface AppData<PageProps extends Inertia.PageProps = Inertia.PageProps> {
-    component: Component | null
-    key: number | null
-    props: PageProps | {}
-  }
-  interface AppProps<
-    PagePropsBeforeTransform extends Inertia.PagePropsBeforeTransform = Inertia.PagePropsBeforeTransform,
-    PageProps extends Inertia.PageProps = Inertia.PageProps
-  > {
-    initialPage: Inertia.Page<PageProps>
-    resolveComponent: (name: string) => Component | Promise<Component>
-    transformProps?: (props: PagePropsBeforeTransform) => PageProps
-  }
-
-  type App<
-    PagePropsBeforeTransform extends Inertia.PagePropsBeforeTransform = Inertia.PagePropsBeforeTransform,
-    PageProps extends Inertia.PageProps = Inertia.PageProps
-  > = Component<
-    AppData<PageProps>,
-    never,
-    never,
-    AppProps<PagePropsBeforeTransform, PageProps>
-  >
-
-  interface InertiaLinkProps {
-    data?: object
-    href: string
-    method?: string
-    onClick?: (event: MouseEvent | KeyboardEvent) => void
-    preserveScroll?: boolean
-    preserveState?: boolean
-    replace?: boolean
-  }
-
-  type InertiaLink = FunctionalComponentOptions<InertiaLinkProps>
+interface AppData<PageProps extends Inertia.PageProps = Inertia.PageProps> {
+  component: Component | null
+  key: number | null
+  props: PageProps | {}
 }
 
-declare module '@inertiajs/inertia-vue' {
-  export const InertiaLink: InertiaVue.InertiaLink
-
-  export const InertiaApp: InertiaVue.App
+interface AppProps<
+  PagePropsBeforeTransform extends Inertia.PagePropsBeforeTransform = Inertia.PagePropsBeforeTransform,
+  PageProps extends Inertia.PageProps = Inertia.PageProps
+> {
+  initialPage: Inertia.Page<PageProps>
+  resolveComponent: (name: string) => Component | Promise<Component>
+  transformProps?: (props: PagePropsBeforeTransform) => PageProps
 }
+
+type App<
+  PagePropsBeforeTransform extends Inertia.PagePropsBeforeTransform = Inertia.PagePropsBeforeTransform,
+  PageProps extends Inertia.PageProps = Inertia.PageProps
+> = Component<
+  AppData<PageProps>,
+  never,
+  never,
+  AppProps<PagePropsBeforeTransform, PageProps>
+>
+
+interface InertiaLinkProps {
+  data?: object
+  href: string
+  method?: string
+  onClick?: (event: MouseEvent | KeyboardEvent) => void
+  preserveScroll?: boolean
+  preserveState?: boolean
+  replace?: boolean
+}
+
+type InertiaLink = FunctionalComponentOptions<InertiaLinkProps>
+
+export const InertiaLink: InertiaLink
+
+export const InertiaApp: App
